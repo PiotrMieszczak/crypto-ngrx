5f992b37562e0404dd7ee5bc312508c21a97002f
Piotr Mieszczak <mieszczak.piotr@gmail.com>
2025-01-29 10:47:48 +0100

    test(task): selector tests

---
+++ src/app/domain/task/state/test/task.selector.spec.ts
@@ -0,0 +1,23 @@
+import { selectTasks, selectLoading, selectError } from '../task.selectors';
+import { TaskState } from '../task.reducer';
+import { mockTasks } from 'src/app/mocks/task.mock';
+
+describe('Task Selectors', () => {
+  const state: TaskState = {
+    tasks: mockTasks,
+    loading: true,
+    error: 'error'
+  };
+
+  it('should select tasks', () => {
+    expect(selectTasks.projector(state)).toEqual(mockTasks);
+  });
+
+  it('should select loading', () => {
+    expect(selectLoading.projector(state)).toBe(true);
+  });
+
+  it('should select error', () => {
+    expect(selectError.projector(state)).toBe('error');
+  });
+});
