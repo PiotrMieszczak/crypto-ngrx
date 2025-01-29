ca9c365fd0a883d93581132cd0f704e4078e7d60
Piotr Mieszczak <mieszczak.piotr@gmail.com>
2025-01-29 13:47:24 +0100

    refactor: move state init to route level

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Route } from '@angular/router';
 import { TaskPageComponent } from './pages/task/task-page.component';
+import { provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { TaskEffects, taskReducer } from './domain/task/state';
 
 export const appRoutes: Route[] = [
     {
-        path: '',
-        component: TaskPageComponent
-      }
+      path: '',
+      component: TaskPageComponent,
+      providers: [
+        provideState({ name: 'tasks', reducer: taskReducer }),
+        provideEffects([TaskEffects])
+      ]
+    }
 ];
